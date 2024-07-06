06471ed191a8beab986f97a339e41eefb9e67bb0
Dato base1 <datocarneiro@outlook.com>
2024-07-06 13:50:03 -0300

    updadte - templates

--- home/views.py
+++ home/views.py
@@ -1,7 +1,6 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
 # Create your views here.
 def home(request):
-    return HttpResponse('estamos na View "HOME" do app home')
+    return render(request, 'home/index.html')
 
